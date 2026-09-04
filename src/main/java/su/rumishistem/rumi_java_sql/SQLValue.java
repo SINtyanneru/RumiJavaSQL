bad17c8ee6e8424d93834999ec7b724ac1f3b468
SINtyanneru <service@rumi-room.net>
2026-09-04 20:19:14 +0900

    DATE型、DATETIME型対応

--- src/main/java/su/rumishistem/rumi_java_sql/SQLValue.java
+++ src/main/java/su/rumishistem/rumi_java_sql/SQLValue.java
@@ -1,7 +1,11 @@
 package su.rumishistem.rumi_java_sql;
 
 import java.sql.Blob;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class SQLValue {
 	private final Object value;
@@ -52,5 +56,31 @@ public class SQLValue {
 
 	public boolean as_boolean() {
 		return (boolean) value;
+	}
+
+	public LocalDateTime as_datetime() {
+		LocalDateTime ldt;
+		if (value instanceof LocalDateTime) {
+			ldt = (LocalDateTime)value;
+		} else if (value instanceof Timestamp) {
+			ldt = ((Timestamp)value).toLocalDateTime();
+		} else {
+			throw new IllegalStateException("予期しない型: " + value.getClass());
+		}
+
+		return ldt;
+	}
+
+	public LocalDate as_date() {
+		LocalDate ld;
+		if (value instanceof LocalDate) {
+			ld = (LocalDate)value;
+		} else if (value instanceof Date) {
+			ld = ((Date)value).toLocalDate();
+		} else {
+			throw new IllegalStateException("予期しない型: " + value.getClass());
+		}
+
+		return ld;
 	}
 }
